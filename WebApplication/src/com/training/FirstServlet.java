f694481fd331c9808d721b94e89f93cc4a608b6f
AC31848 <AC31848@SGH526RPS4.ctl.intranet>
2018-09-28 13:04:21 +0530

    first servlet

---
+++ WebApplication/src/com/training/FirstServlet.java
@@ -0,0 +1,51 @@
+package com.training;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FirstServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// response.setContentType("text/html");
+		//
+		// PrintWriter out = response.getWriter();
+		// out.println("Dynamic Web Project");
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("welcome.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
