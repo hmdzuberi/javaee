854a721ccf8694b56f5e9a99f02d2fb6adefb97c
Hamaad <hamaadz@outlook.com>
2018-10-08 11:46:48 +0530

    initial config

---
+++ REST_WebService/src/com/training/resources/QuickStart.java
@@ -0,0 +1,18 @@
+package com.training.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/quick")
+public class QuickStart {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage() {
+
+		return "You have configured JERSEY properly";
+	}
+
+}
